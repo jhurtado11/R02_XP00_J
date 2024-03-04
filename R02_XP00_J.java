8ec705584d2811a2583c8215e67f6d7d28cceb37
Joel Hurtado <124201675+jhurtado11@users.noreply.github.com>
2024-03-03 23:35:12 -0500

    Add files via upload

---
+++ R02_XP00_J.java
@@ -0,0 +1,5 @@
+p u b l i c v o i d d e l e t e F i l e ( ) {
+F i l e s o m e F i l e = new F i l e ( ” someFileName . t x t ” ) ;
+// Do something with s o m e F i l e
+s o m e F i l e . d e l e t e ( ) ;
+}
